6c1634ad04aa4473c6d2baf31e23ceb29f7759bb
Matthew <thegalah@gmail.com>
2024-03-29 03:33:02 -0700

    Update main.rs

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use chrono::Local;
+
+async fn time() -> impl Responder {
+    HttpResponse::Ok().body(Local::now().to_string())
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/time", web::get().to(time))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
